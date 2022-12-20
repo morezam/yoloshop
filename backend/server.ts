5b63d24a1954dbbac8a8f5df5c20d763e9420c89
Mohammad Hamzehei <mohammadreza.ha.va@gmail.com>
2022-12-20 15:21:06 +0330

    fix port typo

--- backend/server.ts
+++ backend/server.ts
@@ -12,7 +12,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware';
 
 dotenv.config();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 dbConnect();
 const app = express();
